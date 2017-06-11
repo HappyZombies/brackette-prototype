3e1e5fb2509827443e6b6368c1f8fc8a61db359c
Daniel <daniel.reguero@hotmail.com>
2017-06-10 20:58:54 -0400

    Updated Footer to contain version number and other info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import {Footer} from 'react-materialize'
-import '../css/Footer.css'
 
 export default () => {
   return (
     <Footer
-      copyrights='&copy; 2017 Copyright Text'
+      copyrights='&copy; 2017 Copyright Brackette'
       moreLinks={
-        <a className='grey-text text-lighten-4 right' href='https://github.com/HappyZombies/brackette' target='_blank' rel='noopener noreferrer'>Source Code</a>
+          <a className='grey-text text-lighten-4 right' href='https://github.com/HappyZombies/brackette/releases/tag/v0.2.1b' target='_blank' rel='noopener noreferrer'>v0.2.1b</a>
         }
       className='amber accent-2'
-      />
+      links = {
+        <ul>
+          <li><a className='grey-text text-lighten-4 right' href='https://github.com/HappyZombies/brackette' target='_blank' rel='noopener noreferrer'>Source Code</a></li>
+        </ul>
+      }
+      >
+      <h5 className="white-text">Brackette</h5>
+      <p className="grey-text text-lighten-4">Run your tournaments with ease.</p>
+      </Footer>
   )
 }
